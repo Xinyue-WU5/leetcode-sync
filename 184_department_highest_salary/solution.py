3eb8c3d828a86945a38c9c1b23eb9ce532e59d92
Xinyue_Wu <85117100+Xinyue-WU5@users.noreply.github.com>
2025-09-22 14:55:38 -0700

    update solutions to leetcode-Sep222025

---
+++ 184_department_highest_salary/solution.py
@@ -0,0 +1,12 @@
+import pandas as pd
+
+def department_highest_salary(employee: pd.DataFrame, department: pd.DataFrame) -> pd.DataFrame:
+    employee = employee.rename(columns={'name':'Employee'})
+    employee = employee.rename(columns={'salary':'Salary'})
+    department = department.rename(columns={'name':'Department'})
+    df = employee.merge(department, how='left', left_on='departmentId', right_on='id')
+    df['rank'] = df.groupby('departmentId')['Salary'].rank(method="dense", ascending=False)
+
+    salary = df[df['rank']==1]
+
+    return salary[['Department', 'Employee', 'Salary']]
---
+++ 185_department_top_three_salaries/solution.sql
@@ -0,0 +1,11 @@
+SELECT
+    Department,
+    Employee,
+    Salary
+FROM (SELECT
+        d.name AS Department,
+        e.name AS Employee,
+        salary AS Salary,
+        DENSE_RANK() OVER(PARTITION BY e.departmentId ORDER BY e.salary DESC) AS sarank
+    FROM Employee AS e LEFT JOIN Department AS d ON e.departmentId = d.id) AS deem
+WHERE sarank <= 3;
